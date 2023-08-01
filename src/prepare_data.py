01e39305bbd6841cc1980a2176c2271e9cff7b23
adam <oleksakadam@gmail.com>
2023-08-01 10:09:27 +0200

    add example videos, refactor of the preprocessing data function

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -11,81 +11,133 @@ import shutil
 import random
 import pandas as pd
 from collections import Counter
-from utils import get_image, crop_and_augment, delete_files, preprocess_1, preprocess_2
-
-video_path = "example_image/"
-extracted_images_path = "example_image/augmented_images_and_labels/"
-cropped_only_path = "example_image/cropped_only_images_and_labels/"
-
-# get all video files
-all_video = os.listdir(video_path)
-all_video = [i for i in all_video if i.endswith('.AVI')] # sometimes mp3 instead of AVI
-all_video = [x[:-4] for x in all_video]
-all_video.sort()
-
-# extract images from videos
-[get_image(video_path,
-          x,
-          res_dir = extracted_images_path) for x in all_video]
-
-# augment images 
-crop_and_augment(extracted_images_path)
-
-# drop originally cropped images:
-delete_files(extracted_images_path + '*')
-
-# copy cropped images to different dir to annotate them
-all_cropped = os.listdir(extracted_images_path)
-all_cropped = [x for x in all_cropped if 'cropped_resized.jpg' in x]
-
-for file_name in all_cropped:
-    full_file_name = os.path.join(extracted_images_path, file_name)
-    if os.path.isfile(full_file_name):
-        shutil.move(full_file_name, cropped_only_path)
+from utils import get_frame, crop_and_augment, delete_files, preprocess_1, preprocess_2
 
 
-# now it's time to make annotations for images in cropped_only_path
-# select all annotations and annotated images
-all_annotated = os.listdir(cropped_only_path)
-
-# get only jpg annotated names
-jpg_files_annotated = [x[:-3] + "jpg" for x in all_annotated]
-jpg_files_annotated = list(Counter(jpg_files_annotated))
-
-# manage annotations
-all_annotated = os.listdir(cropped_only_path)
-# remove possible existing 'classes.txt" file
-
-annoattions = [x for x in all_annotated if 'cropped_resized.txt' in x]
-cropped_only_path
-# select annotated files and autimatically create annotation files
-# manage non flipped coordinates 
-for file_name in annoattions:
-    new_name = file_name[:-11]+ "contrasted.txt"
-    new_name = os.path.join(extracted_images_path, new_name)
-    shutil.copy(os.path.join(cropped_only_path,  file_name), 
-                new_name)
+def main():   
+    video_path = "../example_image/"
+    augmented_images_path = "../example_image/augmented_images_and_labels/"
+    cropped_only_path = "../example_image/cropped_only_images_and_labels/"
     
-# manage flipped coordinates by changing one of the coordinates 
-for file_name in annoattions:
-    new_name = file_name[:-11]+ "sharped.txt"
-    new_name = os.path.join(extracted_images_path, new_name)
-    shutil.copy(os.path.join(cropped_only_path,  file_name), 
-                new_name)
+    training_set_path_images = "../example_image/train/images"
+    training_set_path_labels = "../example_image/train/labels"
+    
+    validation_set_path_images = "../example_image/val/images"
+    validation_set_path_labels = "../example_image/val/labels"
+    
+    # get all video files
+    all_video = os.listdir(video_path)
+    all_video = [i for i in all_video if i.endswith('.mp4')] # sometimes .AVI instead of .mp4
+    all_video = [x[:-4] for x in all_video]
+    
+    # extract images from videos
+    [get_frame(image_dir = video_path,
+              vid_name = video_name,
+              res_dir = augmented_images_path,
+              video_format = ".mp4",
+              frameRate = 10) for video_name in all_video]
+    
+    
+    # augment images 
+    crop_and_augment(augmented_images_path, 13.5)
+    
+    # drop originally cropped images:
+    delete_files(augmented_images_path + '*')
+    
+    # copy cropped images to different directory to annotate them
+    all_cropped = os.listdir(augmented_images_path)
+    all_cropped = [image for image in all_cropped if 'cropped_resized.jpg' in image]
+    
+    for file_name in all_cropped:
+        full_file_name = os.path.join(augmented_images_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.move(full_file_name, cropped_only_path)
+    
+    
+    # now it's time to make annotations for images in cropped_only_path
+    # select all annotations and annotated images
+    all_annotated = os.listdir(cropped_only_path)
+    annoattions = [x for x in all_annotated if 'cropped_resized.txt' in x]
+    
+    # create annotations for non flipped images 
+    for file_name in annoattions:
+        new_name = file_name[:-11] + "contrasted.txt"
+        new_name = os.path.join(augmented_images_path, new_name)
+        shutil.copy(os.path.join(cropped_only_path,  file_name), 
+                    new_name)
         
-# manage flipped coordinates by changing one of the coordinates 
-for file_name in annoattions:
-    data_raw = pd.read_csv(cropped_only_path + file_name,
-                       header = None)
+    # create annotations for non flipped images 
+    for file_name in annoattions:
+        new_name = file_name[:-11] + "sharped.txt"
+        new_name = os.path.join(augmented_images_path, new_name)
+        shutil.copy(os.path.join(cropped_only_path,  file_name), 
+                    new_name)
+            
+    # create annotations for flipped images 
+    for file_name in annoattions:
+        data_raw = pd.read_csv(cropped_only_path + file_name,
+                           header = None)
+        
+        data_raw = data_raw.apply(lambda x: preprocess_1(x[0]), axis=1)
+        data_raw = data_raw.to_frame()
+        
+        data_raw = data_raw.apply(lambda x: preprocess_2(x[0]), axis=1)
+        data_raw = data_raw.to_frame()
+        
+        data_raw.to_csv(augmented_images_path + file_name[:-11]  + "filpped.txt", header=None, index=None)
+        data_raw.to_csv(augmented_images_path + file_name[:-11]  + "filter2d.txt", header=None, index=None)
+        
+    # copy all files back - from cropped_only_path to augmented_images_path
     
-    x = data_raw.apply(lambda x: preprocess_1(x[0]), axis=1)
-    x = x.to_frame()
+    # getting all the files in the source directory
+    cropped_resized_files = os.listdir(cropped_only_path)
+    for file_name in cropped_resized_files:
+        full_file_name = os.path.join(cropped_only_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, augmented_images_path)
     
-    x2 = x.apply(lambda x: preprocess_2(x[0]), axis=1)
-    x3 = x2.to_frame()
     
-    x3.to_csv(extracted_images_path + file_name[:-11]  + "filpped.txt", header=None, index=None)
-    x3.to_csv(extracted_images_path + file_name[:-11]  + "filter2d.txt", header=None, index=None)
+    
+    # todo: create proper val, train, test set split, to avoid having images from the same video in different sets,
+    # here simple split as in the example we have only two videos
+    all_data = os.listdir(augmented_images_path)
+    train = [i for i in all_data if i.startswith(all_video[0])]
+    train_images = [i for i in train if i.endswith(".jpg")]
+    train_images.append("classes.txt")
+    
+    train_labels = [i for i in train if i.endswith(".txt")]
+    train_labels.append("classes.txt")
+    
+    val = [i for i in all_data if i.startswith(all_video[1])]
+    val_images = [i for i in val if i.endswith(".jpg")]
+    val_images.append("classes.txt")
+    
+    val_labels = [i for i in val if i.endswith(".txt")]
+    val_labels.append("classes.txt")
+    
+    # copy files to train and val directories
+    for file_name in train_images:
+        full_file_name = os.path.join(augmented_images_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, training_set_path_images)
+            
+    for file_name in train_labels:
+        full_file_name = os.path.join(augmented_images_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, training_set_path_labels)
+            
+    for file_name in val_images:
+        full_file_name = os.path.join(augmented_images_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, validation_set_path_images)
+            
+    for file_name in val_labels:
+        full_file_name = os.path.join(augmented_images_path, file_name)
+        if os.path.isfile(full_file_name):
+            shutil.copy(full_file_name, validation_set_path_labels)
+            
+  # todo: create proper val, train, test set split, to avoid having images from the same video in different sets
 
 
-# todo: create proper val, train, test set split, to avoid having images from the same video in different sets
+if __name__ == "__main__":
+    main()
--- src/utils.py
+++ src/utils.py
@@ -18,15 +18,15 @@ import glob
 kernel = np.array([[-1, -1, -1],[-1, 8, -1],[-1, -1, 0]], np.float32) 
 kernel = 1/3 * kernel
 
-def crop_and_augment(path):
+def crop_and_augment(path, crop_value_from_bottom_in_percents=14):
     dirs = os.listdir(path)
     for item in dirs:
-        fullpath = os.path.join(path,item)   
-       # fullpath2 = os.path.join(path2,item)   #corrected
+        fullpath = os.path.join(path, item)   
         if os.path.isfile(fullpath):
             im = Image.open(fullpath)
             f, e = os.path.splitext(fullpath)
-            imCrop = im.crop((0, 0, 1920, 950)) #corrected sometimes 1390, sometimes 950, needs to be automatized
+            new_size = im.size[1] * (100-crop_value_from_bottom_in_percents)/100
+            imCrop = im.crop((0, 0, 1920, new_size))
 
             # resize
             imCrop2 = imCrop.resize((640, 640))
@@ -56,38 +56,41 @@ def crop_and_augment(path):
 
 def preprocess_1(x):
     x = list(x.split(" "))
-    
     return x
+
 
 def preprocess_2(x):
     x = [float(x1) for x1 in x]
     x[1] = 1-x[1]
     x[0] = int(x[0])
-    x= ' '.join(str(e) for e in x)
-
+    x = ' '.join(str(e) for e in x)
     return x
 
-def get_image(image_dir, image_name, res_dir):
-    count=1
-    #for vid in listing:
-    vid_name = image_name
-    vid = image_dir + vid_name + ".AVI"
-    print(vid)
-    vidcap = cv2.VideoCapture(vid)
-    def get_frame(sec):
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-        hasFrames,image = vidcap.read()
-        if hasFrames:
-            cv2.imwrite(res_dir + unidecode(vid_name) + "_" + str(count) +".jpg", image) # Save frame as JPG file
-        return hasFrames
-    sec = 0
-    frameRate = 10 # Change this number to 1 for each 1 second
-    success = get_frame(sec)
-    while success:
-        count = count + 1
-        sec = sec + frameRate
-        sec = round(sec, 2)
-        success = get_frame(sec)
+
+def get_image(func):
+    def wrapper(*args, **kwargs):
+        vid = kwargs.get('image_dir') + kwargs.get('vid_name') + kwargs.get('video_format')# ".AVI" #".mp4"
+        vidcap = cv2.VideoCapture(vid)
+        count = 1
+        sec = 0
+        frameRate = kwargs.get('frameRate')
+        success = func(*args, **kwargs, vidcap = vidcap, count = count, sec = sec)
+        video_length_in_seconds = vidcap.get(cv2.CAP_PROP_FRAME_COUNT) / vidcap.get(cv2.CAP_PROP_FPS)
+        while (success and sec < (video_length_in_seconds - frameRate)):          
+            count = count + 1
+            sec =  round(sec + frameRate, 2)
+            success = func(*args, **kwargs, vidcap = vidcap, count = count, sec = sec)
+    return wrapper
+
+
+@get_image
+def get_frame(image_dir, vid_name, res_dir, vidcap, count, sec, frameRate, video_format, **kwargs):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, sec*1000)
+    hasFrames, image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite(os.path.join(res_dir, unidecode(vid_name) + "_" + str(count) +".jpg"), image) # Save frame as JPG file
+    return hasFrames
+
 
 def delete_files(path_to_rem):
     files = glob.glob(path_to_rem)
